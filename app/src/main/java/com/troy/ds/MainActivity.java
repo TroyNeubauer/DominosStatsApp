2aadadb7fc62dcfc3887d15a44f999e41bc6ba06
Troy Neubauer <troyneubauer@gmail.com>
2020-02-05 07:56:32 -0500

    Added location getter

--- app/src/main/java/com/troy/ds/MainActivity.java
+++ app/src/main/java/com/troy/ds/MainActivity.java
@@ -1,16 +1,25 @@
 package com.troy.ds;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,16 +32,6 @@ public class MainActivity extends AppCompatActivity
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view)
-			{
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
 	}
 
 	@Override
@@ -58,5 +57,59 @@ public class MainActivity extends AppCompatActivity
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	private static final int GPS_REQ_CODE = 0;
+	private static final String TAG = "dominos";
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		if (requestCode == GPS_REQ_CODE) {
+			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				gpsFillImpl();
+			} else {
+				gpsFill(null);
+			}
+		}
+
+	}
+
+
+	public void gpsFillImpl()
+	{
+		LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		try
+		{
+			if (lm == null) throw new NullPointerException();
+			Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			if (location == null) throw new NullPointerException();
+
+			((EditText) findViewById(R.id.latitude)).setText(String.format(Locale.getDefault(), "%f", location.getLongitude()));
+			((EditText) findViewById(R.id.longitude)).setText(String.format(Locale.getDefault(), "%f", location.getLatitude()));
+		}
+		catch (NullPointerException e)
+		{
+			Toast.makeText(this, "Failed to get location", Toast.LENGTH_SHORT).show();
+		}
+		catch (SecurityException e)
+		{
+			Toast.makeText(this, "Permissions failed. Code logic error", Toast.LENGTH_SHORT).show();
+		}
+
+	}
+
+	public void gpsFill(View view)
+	{
+		Log.i(TAG, "Calling gpsfill");
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+		{
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, GPS_REQ_CODE);
+		}
+		else
+		{
+			gpsFillImpl();
+		}
+
+
 	}
 }
