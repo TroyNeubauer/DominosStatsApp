63c8ed6739fb2a4e273ebd30808dd4d4ee9b3e47
Troy Neubauer <troyneubauer@gmail.com>
2020-02-05 06:14:05 -0500

    Added default android studio files

--- app/src/androidTest/java/com/troy/ds/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/troy/ds/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.troy.ds;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -15,12 +16,14 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+public class ExampleInstrumentedTest
+{
+	@Test
+	public void useAppContext()
+	{
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.troy.ds", appContext.getPackageName());
-    }
+		assertEquals("com.troy.ds", appContext.getPackageName());
+	}
 }
